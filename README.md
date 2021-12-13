99f65cc8eaba2b764ad28fc9f0b102830dfe2dcb
Martin Van Aken <martin@joyouscoding.com>
2021-12-13 15:41:12 +0100

    Add small test to retreive a feature based on a point

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ This uses Python but should be easily translated in any other language.
 pip install -r requirements.txt
 python who_gis_api.py
 ```
+
+## Methods
+
+Two methods are demonstrated:
+
+- One to get a list of features based on a where clause
+- One to get the feature surrounding a given GPS point given its coordinates
+
+The second one could allow data collected at a much lower level to be "attached" to one of the three standard level - assuming GPS coordinates are available, it could be as simple as "give me the feature that contains this point".
--- client.py
+++ client.py
@@ -4,7 +4,21 @@ BASE_ADM0_URL = "https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/servic
 BASE_ADM1_URL = "https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Detailed_Boundary_ADM1/FeatureServer/0/query"
 BASE_ADM2_URL = "https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Detailed_Boundary_ADM2/FeatureServer/0/query"
 
-
+def get_gis_from_point(point, level):
+  base_url = f"https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Detailed_Boundary_{level}/FeatureServer/0/query"
+  lat = point['lat']
+  long = point['long']
+  params = {
+    'f': 'json',
+    'where': '1=1',
+    'outFields': '*',
+    'geometryType': 'esriGeometryPoint',
+    'geometry': f'{long},{lat}',
+  }
+  response = requests.get(base_url, params=params)
+  data = response.json()
+  features = data['features']
+  return features
 
 def get_gis_data(country_code, level):
   base_url = f"https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Detailed_Boundary_{level}/FeatureServer/0/query"
@@ -34,3 +48,7 @@ sub2_level_features = get_gis_data("BE", "ADM2")
 
 belgium_adm2 = sub2_level_features[0]
 pprint(belgium_adm2)
+
+where_is_this = get_gis_from_point({"lat": 50.85, "long": 4.35}, "ADM2")
+
+pprint(where_is_this)
